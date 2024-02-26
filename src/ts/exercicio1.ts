cfaf477d14a45753bf4c2a6633c24943a32b14b1
DevLucas <luksilvafe@gmail.com>
2024-02-26 17:22:48 -0300

    26/02/24

--- src/ts/exercicio1.ts
+++ src/ts/exercicio1.ts
@@ -1,21 +1,26 @@
 //Hospital chega doente sai bom
-class Pessoa {
+abstract class Pessoa {
     constructor(
-        public nome: string,
+        private nome: string,
         public endereco: string,
-        public email: string,
+        private email: string,
         public telefone: string,
         public data_nascimento: Date,
-        public rg: string
+        protected rg: string
     ) { }
     mostraDados() {
-        console.log(`Dado da Pessoa:`)
-        console.log(`Nome: ${this.nome}`)
-        console.log(`Endereco: ${this.endereco}`)
-        console.log(`RG: ${this.rg}`)
-        console.log(`E-mail: ${this.email}`)
-        console.log(`Telefone: ${this.telefone}`)
-        console.log(`Data nascimento: ${this.data_nascimento.toLocaleDateString('pt-br')}`)
+
+    }
+    getEmail(): string {
+        return this.email
+    }
+    getNome(): string {
+        return this.nome
+    }
+    //Modificar
+    setEmail(email: string): void {
+        //this email recebe email
+        this.email = email
     }
 }
 
@@ -32,58 +37,67 @@ class Funcionario extends Pessoa {
         super(nome, endereco, email, telefone, data_nascimento, rg)
     }
     mostraDados() {
-        super.mostraDados()
+        console.log(`--Dado da Pessoa--`)
+        console.log(`Nome: ${super.getNome()}`)
+        console.log(`Endereco: ${this.endereco}`)
+        console.log(`RG: ${this.rg}`)
+        console.log(`E-mail: ${super.getEmail()}`)
+        console.log(`Telefone: ${this.telefone}`)
+        console.log(`Data nascimento: ${this.data_nascimento.toLocaleDateString('pt-br')}`)
         console.log(`Cargo: ${this.cargo}`)
-        console.log(`Matricula: ${this.matricula}`)
+        console.log(`Matricula: ${this.matricula}\n`)
     }
 }
-const funcionario1 = new Funcionario('Concertardor de geladeiras', 'sla', 'Jasinto', 'Rua dos bobos n°0', 'funcionario@hotmail.com', '(84)923525552', new Date('1995-11-11'), '234324')
-console.log(funcionario1.mostraDados())
+const funcionario1 = new Funcionario('Concertardor de geladeiras', 'sla', 'Jasinto', 'Rua dos bobos n°0', 'funcionario@hotmail.com', '(84)92352-5552', new Date('1995-11-11'), '234324')
+funcionario1.mostraDados()
+funcionario1.setEmail('lucasMod@gmail.com')
+funcionario1.getEmail()
 
 
 class Consultas {
     constructor(
         public local: string,
         public data: Date,
-        public convenio: boolean,
+        protected convenio: boolean,
         public medico: string
-        ) {}
+    ) { }
     mostraDados() {
-        console.log(`Dado da Consulta:`)
+        console.log(`--Dado da Consulta--`)
         console.log(`Data: ${this.data}`)
         console.log(`Local: ${this.local}`)
         console.log(`Convenio: ${this.convenio}`)
-        console.log(`Medico: ${this.medico}`)
+        console.log(`Medico: ${this.medico}\n`)
     }
 }
 
-const consulta1 = new Consultas('Lagoa Azul', new Date('2024-2-19'), false, 'Lucas')
-console.log(consulta1.mostraDados())
+const consulta1 = new Consultas('Lagoa Azul', new Date('2024-2-19'), false, 'Med Lucas')
+consulta1.mostraDados()
 
 class Paciente extends Pessoa {
     constructor(
         public cpf: string,
         public crtaosus: string,
-        public obs_alergias: string,
+        protected alergias: string,
         nome: string,
         endereco: string,
         email: string,
         telefone: string,
         data_nascimento: Date,
         rg: string
-        ) {
-        super(nome, endereco, email, telefone, data_nascimento, rg)
-
-    }
+    ) { super(nome, endereco, email, telefone, data_nascimento, rg) }
     mostrarDados() {
-        console.log(`Dado do Pciente:`)
-        console.log(`E-mail: ${this.cpf}`)
-        console.log(`Telefone: ${this.crtaosus}`)
-        console.log(`cargo: ${this.endereco}`)
-        console.log(`Data nascimento: ${this.endereco}`)
-        console.log(`Data nascimento: ${this.obs_alergias}`)
+        console.log(`--Dado do Pciente--`)
+        console.log(`CPF: ${this.cpf}`)
+        console.log(`Cartão: ${this.crtaosus}`)
+        console.log(`Alergico: ${this.alergias}`)
+        console.log(`Nome: ${super.getNome()}`)
+        console.log(`Endereco: ${this.endereco}`)
+        console.log(`Telefone: ${this.telefone}`)
+        console.log(`Data de nascimento: ${this.data_nascimento.toLocaleString('pt-BR')}`)
+        console.log(`RG: ${this.rg}\n`)
     }
 }
-const paciente = new Paciente('Lucas Ferreira', 'Av piratininga', 'luksilvafe@gmail.com', '3646643464', 'rerrre', '43324432', '097423432434', new Date ('2000-11-11'), '2343243')
-console.log(paciente.mostraDados())
-
+const paciente1 = new Paciente('cpf', 'numero sus', 'nao', 'lucas', 'av', '@gmail', '(84)94565-7748', new Date('2000-11-11'), '32424')
+paciente1.mostrarDados()
+/* Analisar as questões 1,2,3 e 4 buscando atributos que precisam serem privados ou protect.
+Buscar nos exercicios as classes que precisam de abstração*/
--- src/ts/exercicio2.ts
+++ src/ts/exercicio2.ts
@@ -1,65 +1,60 @@
 // Leitura Certa
 class CadastroLivro {
-    nome: string;
-    autor: string;
-    publicacao: Date;
-    // Metodo construtor
-    constructor(nome: string, autor: string, publicacao: Date)
-    {
-        this.nome = nome;
-        this.autor = autor;
-        this.publicacao = publicacao;
-    }
-    cadastroLivro()
-    {
-        console.log(`Cadastro de Livros`)
-        console.log(`Nome: ${this.nome}`)
-        console.log(`Autor: ${this.autor}`)
-        console.log(`Data de Publicação: ${this.publicacao}`)
-    }
+    constructor(
+        public nome: string,
+        public autor: string,
+        public publicacao: Date
+    ) { }
+    cadastroLivro() { }
 }
-const cadastrolivro1 = new CadastroLivro('Clean Code', 'Robert Cecil Martin', new Date('2008-7-8'))
-console.log(cadastrolivro1.cadastroLivro())
 
-class CadastroUsuario {
-    nome: string;
-    cpf: string;
-    telefone: string;
-    endereco: any[];
-    // Metodo construtor
-    constructor(nome: string, cpf: string, telefone: string, endereco: any[])
-    {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.telefone = telefone;
-        this.endereco = endereco;
-    }
-    cadastroUsuario()
-    {
-        console.log('Cadastro do Usuario')
-        console.log(`Nome do `)
-    }
+class Usuario {
+    constructor(
+        public nome: string,
+        public telefone: string,
+        public email: string,
+        protected cpf: string,
+        protected endereco: any[]
+    ) { }
+    dadosUsuario() { }
 }
-const cadastrousuario1 = new CadastroUsuario('Lucas', '3353925524', '46663646', ['Rua quadrada', 'Av. redonda', 234])
-console.log(cadastrousuario1)
 
-class EmprestimoLivro {
-    nome: string;
-    cpf: string;
-    telefone: string;
-    quantidade: number;
-    emprestimo: Date;
-    entrega: Date;
-    // Metodo construtor
-    constructor(nome: string, cpf: string, telefone: string, quantidade: number, emprestimo: Date, entrega: Date)
-    {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.telefone = telefone;
-        this.quantidade = quantidade;
-        this.emprestimo = emprestimo;
-        this.entrega = entrega;
+class CadastroUsuario extends Usuario{
+    constructor(
+        nome: string,
+        email: string,
+        cpf: string,
+        telefone: string,
+        endereco: any[]
+    ) { super(nome, telefone, email, cpf, endereco)}
+    cadastroUsuario() {
+        console.log('--Cadastro do Usuario--')
+        console.log(`Nome do Usuário: ${this.nome}`)
+        console.log(`CPF: ${this.cpf}`)
+        console.log(`Telefone: ${this.telefone}`)
+        console.log(`Endereço: ${this.endereco}`)
+        console.log(`Email: ${this.email}`)
     }
 }
-const emprestimolivro1 = new EmprestimoLivro('Lucas', '3353925524', '46663646', 3, new Date('2024-1-3'), new Date('2024-2-2'))
-console.log(emprestimolivro1)
+const cadastrousuario = new CadastroUsuario('Lucas', '123@gmail.com', '46663646', 'sddsd', ['Rua quadrada', 'Av. redonda', 234])
+cadastrousuario.cadastroUsuario()
+
+class EmprestimoLivro extends CadastroLivro {
+    // Metodo construtor
+    constructor(
+        protected cpf: string,
+        public telefone: string,
+        protected quantidade: number,
+        protected data_emprestimo: Date,
+        protected entrega: Date
+    ) { super(nome, autor, publicacao) }
+    mostrarEmpretimo() {
+        console.log(`CPF: ${this.cpf}`)
+        console.log()
+        console.log()
+        console.log()
+        console.log()
+
+    }
+    const emprestimolivro1 = new EmprestimoLivro('Lucas', '3353925524', '46663646', 3, new Date('2024-1-3'), new Date('2024-2-2'))
+emprestimolivro.mostrarEmpretimo()
